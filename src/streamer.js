94b066927bc966e58cb6d442464e402709a84c3b
dcliff9 <dcliff9@gmail.com>
2024-01-30 18:16:31 -0800

    TestVideo Path2

--- src/streamer.js
+++ src/streamer.js
@@ -5,7 +5,7 @@ const {
 let ffmpegProcess; // Define the variable at the top level
 
 function startStreaming(srtUrl, logCallback) {
-    const ffmpegCommand = `ffmpeg -re -i /usr/src/app/public/testvideo.mp4 -c copy -f mpegts ${srtUrl}`;
+    const ffmpegCommand = `ffmpeg -re -i /usr/src/app/src/public/testvideo.mp4 -c copy -f mpegts ${srtUrl}`;
     ffmpegProcess = exec(ffmpegCommand, (error, stdout, stderr) => {
         if (error) {
             logCallback(`Error: ${error.message}`);
