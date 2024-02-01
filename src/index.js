f0dd3aa249315b30f0baa007a65fefa0e261e4b0
dcliff9 <dcliff9@gmail.com>
2024-01-31 21:59:13 -0800

    api 3

--- src/index.js
+++ src/index.js
@@ -16,6 +16,7 @@ app.use(cors({
     credentials: true
 }));
 
+app.use(express.json());
 app.use('/api', apiRoutes); // Mount the API routes under the '/api' path
 
 // Configure multer for custom file naming and directory
